4f00bc0fd9530b0879bf6c197bda79f411056f2d
celalaslan <celalaslan@outlook.com>
2018-04-18 15:22:21 -0700

    project created

---
+++ objective-c-assignment#2/objective-c-assignment#2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  objective-c-assignment#2
+//
+//  Created by Celal Aslan on 2018-04-18.
+//  Copyright © 2018 Celal Aslan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
