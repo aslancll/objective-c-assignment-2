113de6152a3cfa0de35958a7426f070ff7c18251
celalaslan <celalaslan@outlook.com>
2018-04-18 15:24:30 -0700

    Box class created

---
+++ objective-c-assignment#2/objective-c-assignment#2/Box.h
@@ -0,0 +1,13 @@
+//
+//  Box.h
+//  objective-c-assignment#2
+//
+//  Created by Celal Aslan on 2018-04-18.
+//  Copyright © 2018 Celal Aslan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Box : NSObject
+
+@end
---
+++ objective-c-assignment#2/objective-c-assignment#2/Box.m
@@ -0,0 +1,13 @@
+//
+//  Box.m
+//  objective-c-assignment#2
+//
+//  Created by Celal Aslan on 2018-04-18.
+//  Copyright © 2018 Celal Aslan. All rights reserved.
+//
+
+#import "Box.h"
+
+@implementation Box
+
+@end
